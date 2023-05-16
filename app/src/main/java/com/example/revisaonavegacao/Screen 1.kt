6e9c374e2c5dc68bcb753fd14a5d6c7a18139acc
Margareth Sasaki <magasasaki@gmail.com>
2023-05-15 22:28:22 -0300

    Adicionado import

--- app/src/main/java/com/example/revisaonavegacao/Screen 1.kt
+++ app/src/main/java/com/example/revisaonavegacao/Screen 1.kt
@@ -1,11 +1,15 @@
 package com.example.revisaonavegacao
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 
 @Composable
 fun Screen1(navController: NavHostController) {
