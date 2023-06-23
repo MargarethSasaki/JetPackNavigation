c52a539bc187f55ad8b419abd0f166020294c927
Margareth Sasaki <116614775+MargarethSasaki@users.noreply.github.com>
2023-06-22 22:23:51 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/revisaonavegacao/MainActivity.kt
+++ app/src/main/java/com/example/revisaonavegacao/MainActivity.kt
@@ -1,33 +1,51 @@
 package com.example.revisaonavegacao
-
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@Composable
-fun Screen1(navController: NavHostController) {
-    Scaffold(
-        topBar = {
-            TopAppBar { Text(text = "Screen 1") }
-        }
-    ) {
-        // Screen content
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(text = "Screen 1")
-            Button(onClick = {navController.navigate(Destination.ScreenSecond.route)}) {
-               Text(text = "Next")
+import androidx.navigation.compose.rememberNavController
+import com.example.revisaonavegacao.ui.theme.RevisaoNavegacaoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RevisaoNavegacaoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Destination.ScreenFirst.route) {
+                        composable(Destination.ScreenFirst.route) { Screen1(navController = navController )}
+                        composable(Destination.ScreenSecond.route) { Screen2(ControladorDeNavegacao = navController) }
+
+                    }
+                }
             }
+        }
+    }
+}
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RevisaoNavegacaoTheme {
+        Greeting("Android")
+    }
+}
